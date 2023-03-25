f8f894301494662060104e6c5be1058207a88d10
Abishek <abishekhari09@gmail.com>
2023-03-25 21:12:02 +0530

    made some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
       <header className=' w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-        <Link to='/'>
-          <img src={logo} alt='logo' className=' w-28 object-contain' />
+        <Link to='/' className='font-medium text-3xl text-slate-900'>
+          IMAGECRAFT
         </Link>
         <Link
           to='/create-post'
